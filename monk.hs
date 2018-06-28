ad9aff6ae90a4b27ff8f77fef28d8a7889cbd1aa
Tiago Campos <itotimcampos@windowslive.com>
2018-06-27 23:26:26 -0300

    add convergency of lex and fix many bugs....

--- monk.hs
+++ monk.hs
@@ -12,10 +12,10 @@ data Agents = Agent_M Monkey | Agent_P Predator deriving Show
 data Floor = Floor Int (Maybe Agents) deriving Show
 data Floor_map = Floor_map (Matrix Floor) [Pos] deriving Show
 
-size_of_area = 10
-amoung_of_agent_monkey = 2
-predators = 4
-area_length = 6
+size_of_area = 40
+amoung_of_agent_monkey = 10
+predators = 30
+area_length = 4
 len_of_table_symb = 10
 len_of_word_lex = 6
 
@@ -43,6 +43,13 @@ lex__s a = do {rand_lis <- fx len_of_word_lex (97, 122); if a <= 0 then return [
 monkey_generate_lex :: [String] -> [String] -> [String] -> Symb_lex
 monkey_generate_lex lex lex_ lex__ = Symb_lex lex lex_ lex__
 
+lis_lex :: Int -> IO [[String]]
+lis_lex 0 = return []
+lis_lex n = do
+        lex__ <- lex__s len_of_table_symb
+        f <- lis_lex (n - 1)
+        return (lex__ : f)
+
 monkey__l :: Int -> [[String]] -> [[String]] -> [[String]] -> [Monkey]
 monkey__l 0 xs xt xe = []
 monkey__l d xs xt xe = (Monkey d (monkey_generate_lex (head xs) (head xt) (head xe)) (Pos 0 0)) : (monkey__l (d - 1) (tail xs) (tail xt) (tail xe))
@@ -60,12 +67,13 @@ map__floor__  = (Floor_map floor__ [])
 monkey___ :: Matrix Floor -> Monkey -> (Int, Int) -> Matrix Floor
 monkey___ k m_ (x, y) = setElem (Floor 1 (Just (Agent_M (Monkey (id_ m_) (lex_ m_) (Pos x y))))) (x, y) k
 pred__ :: Matrix Floor -> Predator-> (Int, Int) -> Matrix Floor
-pred__ k m_ pp = setElem (Floor 2 (Just (Agent_P m_))) pp k
+pred__ k m_ (x, y) = setElem (Floor 2 (Just (Agent_P (Predator (getPredatorType m_) (Pos x y))))) (x, y) k
 
 floor_attr :: Matrix Floor -> (Int, Int) -> Int
 floor_attr f_ p = get_att (f_ ! p)
 get_att (Floor s d) = s
 get_Pos (Monkey s k z) = z
+get_PPos (Predator s k) = k
 get_maybe_agents :: Floor -> Maybe Agents
 get_maybe_agents(Floor s d) = d
 getPredatorType(Predator x d) = x
@@ -85,28 +93,24 @@ getMonkeyByPos :: Matrix Floor -> Pos -> Monkey
 getMonkeyByPos k (Pos x y) =  get_maybe_monkeys $ get_maybe_agents (k ! (x, y))
 
 put_in_map_floor :: Floor_map -> [Monkey] -> IO (Floor_map)
+put_in_map_floor (Floor_map a p_) [] = return (Floor_map a p_)
 put_in_map_floor (Floor_map a p_) k = do rx <- rand__d (1, size_of_area)
                                          ry <- rand__d (1, size_of_area)
-                                         if (length k) <= 0
-                                           then return (Floor_map a p_)
-                                         else
-                                           if (floor_attr a (rx, ry)) /= 0
-                                             then put_in_map_floor (Floor_map a p_) k
-                                           else do 
-                                             d <- put_in_map_floor (Floor_map a p_) (tail k)
-                                             return (Floor_map (monkey___ (getFloor d) (head k) (rx, ry)) ((Pos rx ry) : (getPos_Agents d)))
+                                         d <- put_in_map_floor (Floor_map a p_) (tail k)
+                                         if (floor_attr (getFloor d) (rx, ry)) /= 0
+                                           then put_in_map_floor (Floor_map a p_) k
+                                         else do 
+                                           return (Floor_map (monkey___ (getFloor d) (head k) (rx, ry)) ((Pos rx ry) : (getPos_Agents d)))
 
 putP_in_map_floor :: Floor_map -> [Predator] -> IO (Floor_map)
+putP_in_map_floor (Floor_map a p_) [] = return (Floor_map a p_)
 putP_in_map_floor (Floor_map a p_) k = do rx <- rand__d (1, size_of_area)
                                           ry <- rand__d (1, size_of_area)
-                                          if (length k) <= 0
-                                            then return (Floor_map a p_)
-                                          else
-                                            if (floor_attr a (rx, ry)) /= 0
-                                              then putP_in_map_floor (Floor_map a p_) k
-                                             else do 
-                                               d <- putP_in_map_floor (Floor_map a p_) (tail k)
-                                               return (Floor_map (pred__ (getFloor d) (head k) (rx, ry)) ((Pos rx ry) : (getPos_Agents d)))
+                                          d <- putP_in_map_floor (Floor_map a p_) (tail k)
+                                          if (floor_attr (getFloor d) (rx, ry)) /= 0
+                                            then putP_in_map_floor (Floor_map a p_) k
+                                           else do 
+                                             return (Floor_map (pred__ (getFloor d) (head k) (rx, ry)) ((Pos rx ry) : (getPos_Agents d)))
 
 
 monkey_xs :: Floor_map -> [Monkey]
@@ -115,6 +119,12 @@ monkey_xs xs
               | (get_att ((getFloor xs) ! (pos_to_tuple ((head (getPos_Agents xs)))))) == 1 = (get_maybe_monkeys (get_maybe_agents ((getFloor xs) ! (pos_to_tuple ((head (getPos_Agents xs))))))) :  (monkey_xs (Floor_map (getFloor xs) (tail (getPos_Agents xs))))
               | otherwise = (monkey_xs (Floor_map (getFloor xs) (tail (getPos_Agents xs))))
 
+predator_xs :: Floor_map -> [Predator]
+predator_xs xs
+              | (length (getPos_Agents xs) ) == 0 = []
+              | (get_att ((getFloor xs) ! (pos_to_tuple ((head (getPos_Agents xs)))))) == 2 = (get_maybe_predator (get_maybe_agents ((getFloor xs) ! (pos_to_tuple ((head (getPos_Agents xs))))))) :  (predator_xs (Floor_map (getFloor xs) (tail (getPos_Agents xs))))
+              | otherwise = (predator_xs (Floor_map (getFloor xs) (tail (getPos_Agents xs))))
+         
 max_or_min_area :: Int -> Int
 max_or_min_area x
                    | x > size_of_area = size_of_area
@@ -172,43 +182,102 @@ alert_in_area f x y z = monkey___ (alert_in_area f x (tail y) z) (alert__monkey 
 
 check_area :: Floor_map -> Monkey -> [Predator] -> Floor_map
 check_area f_ x [] = (Floor_map (getFloor f_) (getPos_Agents f_))
+
 check_area f_ x p = (Floor_map (alert_in_area (getFloor (check_area f_ x (tail p))) x (getMonkey f_ (get_Pos x) area_length) (head p)) (getPos_Agents f_))
+
+monkey_interact :: Floor_map -> Monkey -> Floor_map
+monkey_interact k m = check_area k m (getPredator k (get_Pos m) area_length)
 
 interact__ :: Floor_map -> [Monkey] -> Floor_map
 interact__ k m
                          | (length m) <= 0 = k
-                         | otherwise = check_area k (head m) (getPredator k (get_Pos (head m)) area_length)
+                         | otherwise = monkey_interact (interact__ k (tail m)) (head m)
 
-lis_lex :: Int -> IO [[String]]
-lis_lex 0 = return []
-lis_lex n = do
-        lex__ <- lex__s len_of_table_symb
-        f <- lis_lex (n - 1)
-        return (lex__ : f)
-
-f :: Int -> Symb_lex -> Symb_lex-> Symb_lex
+f :: Int -> Symb_lex -> Symb_lex-> Symb_lex -- Just for tests
 f 0 x y = x
 f n x y = add_lex (f (n - 1 ) x y) y 1
 
+remove_monkey :: Floor_map -> Monkey -> Floor_map
+remove_monkey f m = (Floor_map (setElem (Floor 0 Nothing) (pos_to_tuple (get_Pos m)) (getFloor f)) (getPos_Agents f))
+
+remove_predator :: Floor_map -> Predator -> Floor_map
+remove_predator f m = (Floor_map (setElem (Floor 0 Nothing) (pos_to_tuple (get_PPos m)) (getFloor f)) (getPos_Agents f))
+
+remove_predator_of_map :: Floor_map -> [Predator] -> Floor_map
+remove_predator_of_map f [] = f
+remove_predator_of_map f k = remove_predator (remove_predator_of_map f (tail k)) (head k)
+
+remove_monkey_of_map :: Floor_map -> [Monkey] -> Floor_map
+remove_monkey_of_map f [] = f
+remove_monkey_of_map f k = remove_monkey (remove_monkey_of_map f (tail k)) (head k)
+
+update_monkey :: Floor_map -> [Monkey] -> IO Floor_map
+update_monkey k m = do
+                    let map__ = remove_monkey_of_map k m 
+                    p <- put_in_map_floor map__ m
+                    return p
+                    
+update_predator :: Floor_map -> [Predator] -> IO Floor_map
+update_predator k m = do
+                    let map__ = remove_predator_of_map k m 
+                    p <- putP_in_map_floor map__ m
+                    return p
+
+create_simp :: IO Floor_map
+create_simp = do
+                           let map_floor = map__floor__
+                           lis_posx <- fx amoung_of_agent_monkey (1, size_of_area)
+                           lis_posy <- fx amoung_of_agent_monkey (1, size_of_area)
+                           lis_p <- fx predators (1, 3)
+                           lex__snake <- lis_lex amoung_of_agent_monkey
+                           lex__tiger <- lis_lex amoung_of_agent_monkey
+                           lex__eagle <- lis_lex amoung_of_agent_monkey
+                           let monkeys = monkey__l amoung_of_agent_monkey lex__snake lex__tiger lex__eagle
+                           let predator_ = pred__l predators lis_p
+                           map_env <- put_in_map_floor map_floor monkeys
+                           map_env <- putP_in_map_floor map_env predator_
+                           print_symb_lex (monkey_xs map_env)
+                           return map_env
+
+
+simp ::  Floor_map -> IO Floor_map
+simp map_env = do
+                           let monk_ = monkey_xs map_env
+                           let pred_ = predator_xs map_env
+                           let d = interact__ map_env monk_
+                           let new_map = (Floor_map (getFloor d) [])
+                           new_map <- update_monkey new_map (monkey_xs d)
+                           new_map <- update_predator new_map pred_
+                           return new_map
+
+ai :: Int -> IO Floor_map
+ai 0 = do
+          map_env <- create_simp
+        --  print_symb_lex (monkey_xs map_env)
+          return map_env
+ai n = do
+         ai___ <- ai (n - 1)
+         simp__ <- simp ai___
+         return (simp__)
+
+print_symb_lex :: [Monkey] -> IO ()
+print_symb_lex [] = do
+                    putStrLn ""
+                    putStr ""
+
+print_symb_lex m = do
+                   putStr "Monkey "
+                   print (id_ (head m))
+                   print (snake (lex_ (head m)))
+                   putStrLn ""
+                   print (eagle (lex_ (head m)))
+                   putStrLn ""
+                   print (tiger (lex_ (head m)))
+                   putStrLn "" 
+                   print_symb_lex (tail m)
+
 main :: IO ()
 main = do
-   let lis = ["ola", "bom", "dia", "amigo"]
-   let lis_ = ["ola", "seja", "bem", "vindo"]
-   lis_posx <- fx amoung_of_agent_monkey (1, size_of_area)
-   lis_posy <- fx amoung_of_agent_monkey (1, size_of_area)
-   lis_p <- fx predators (1, 3)
-   lex__snake <- lis_lex amoung_of_agent_monkey
-   lex__tiger <- lis_lex amoung_of_agent_monkey
-   lex__eagle <- lis_lex amoung_of_agent_monkey
-   let monkeys = monkey__l amoung_of_agent_monkey lex__snake lex__tiger lex__eagle
-   let predator_ = pred__l predators lis_p
-   let map_floor = map__floor__
-   map_env <- put_in_map_floor map_floor monkeys
-   map_env <- putP_in_map_floor map_env predator_
-   let d = interact__ map_env 
-   print (lex_ (head monkeys))
-   putStr "\n"
-   print (lex_ (head (tail monkeys)))
-   putStr "\n"
-   let d = f 100 (Symb_lex ["za", "ba"] ["a"] ["b"]) (Symb_lex ["zz"] ["zc"] ["b"])
-   print d
+    env <- ai 1000
+    print "------------------------------------ SIMPS -----------------------------"
+    print_symb_lex $ monkey_xs env
